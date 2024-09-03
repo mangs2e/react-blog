d1f375be06fcdedb421a051f93b8582e2c80e114
Myeongjin <mangs2e@naver.com>
2024-09-03 22:12:59 +0900

    실습코드 정리

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useState } from 'react';
 //아래 언어는 사실 html이 아니라 JSX
 function App() {
 
-  let [[title1, title2, title3], b] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
+  let [title, b] = useState(['남자코트 추천', '강남 우동맛집', '파이썬독학']);
 
   return (
     <div className="App">
@@ -14,18 +14,18 @@ function App() {
         <h4>ReactBlog</h4>
       </div>
       {/* 블로그 글 리스트 만들기 */}
-      <ul className="list">
-        <h4>{title1}</h4>
+      <div className="list">
+        <h4>{title[0]}</h4>
         <p>2월 17일 발행</p>
-      </ul>
-      <ul className="list">
-        <h4>{title2}</h4>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
         <p>2월 17일 발행</p>
-      </ul>
-      <ul className="list">
-        <h4>{title3}</h4>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
         <p>2월 17일 발행</p>
-      </ul>
+      </div>
     </div>
   );
 }
