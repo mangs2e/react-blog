394971d00aa0009cca96b85fd7caad3a6c7de780
Myeongjin <mangs2e@naver.com>
2024-09-09 23:44:14 +0900

    component 기본문법

--- src/App.js
+++ src/App.js
@@ -49,8 +49,22 @@ function App() {
         <h4>{title[2]}</h4>
         <p>2월 17일 발행</p>
       </div>
+
+      {/* 모달 상세페이지 컴포넌트 생성 */}
+      <Modal></Modal>
+      
     </div>
   );
+}
+
+function Modal() {
+  return (
+    <div className='modal'>
+      <h4>제목</h4>
+      <p>내용</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
